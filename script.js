f01e75949e5721ecfaaa6fe4027bf512dffa2ae6
CloudSteak <32801432+the1bit@users.noreply.github.com>
2022-05-10 17:02:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 let allUsers = document.getElementsByClassName("ms-listviewtable")[0].childNodes[1].childNodes
 let userTable = `<table><tr><th>Name</th><th>Type</th><th>Permission level</th></tr>`;
 
-console.log("Names");
+let names = "";
 for (let u = 1; u < allUsers.length; u++) {
     try {
         let userName = document.getElementsByClassName("ms-listviewtable")[0].childNodes[1].childNodes[u].childNodes[2].childNodes[1].childNodes[1].childNodes[0].childNodes[1].innerText;
-        console.log(userName);
+        names+= `${userName} ,`;
         let userType = document.getElementsByClassName("ms-listviewtable")[0].childNodes[1].childNodes[u].childNodes[3].childNodes[1].value;
         let userPermissionLevel = document.getElementsByClassName("ms-listviewtable")[0].childNodes[1].childNodes[u].childNodes[4].childNodes[0].data.toString();
         userTable += `<tr><td>${userName}</td><td>${userType}</td><td>${userPermissionLevel}</td></tr>`;
@@ -18,3 +18,4 @@ for (let u = 1; u < allUsers.length; u++) {
 }
 userTable += `</table>`;
 console.log(userTable);
+console.log(names);
